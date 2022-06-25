2eac9b3b5f82ca4dbbd7be760c89d10c3f69e5bf
SUETAK <akasirotonbo08451@gmail.com>
2022-06-26 01:46:40 +0900

    proto ファイルの変更を追従できるのかを確認する

--- src/pkg/grpc/myapp.pb.go
+++ src/pkg/grpc/myapp.pb.go
@@ -20,18 +20,113 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
+type VoteRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Who string `protobuf:"bytes,1,opt,name=who,proto3" json:"who,omitempty"`
+}
+
+func (x *VoteRequest) Reset() {
+	*x = VoteRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_myapp_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *VoteRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*VoteRequest) ProtoMessage() {}
+
+func (x *VoteRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_myapp_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use VoteRequest.ProtoReflect.Descriptor instead.
+func (*VoteRequest) Descriptor() ([]byte, []int) {
+	return file_myapp_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *VoteRequest) GetWho() string {
+	if x != nil {
+		return x.Who
+	}
+	return ""
+}
+
+type VoteResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Winner string `protobuf:"bytes,1,opt,name=winner,proto3" json:"winner,omitempty"`
+}
+
+func (x *VoteResponse) Reset() {
+	*x = VoteResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_myapp_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *VoteResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*VoteResponse) ProtoMessage() {}
+
+func (x *VoteResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_myapp_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use VoteResponse.ProtoReflect.Descriptor instead.
+func (*VoteResponse) Descriptor() ([]byte, []int) {
+	return file_myapp_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *VoteResponse) GetWinner() string {
+	if x != nil {
+		return x.Winner
+	}
+	return ""
+}
+
 type HelloRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
 	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	Old  int32  `protobuf:"varint,2,opt,name=old,proto3" json:"old,omitempty"`
 }
 
 func (x *HelloRequest) Reset() {
 	*x = HelloRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_myapp_proto_msgTypes[0]
+		mi := &file_myapp_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -44,7 +139,7 @@ func (x *HelloRequest) String() string {
 func (*HelloRequest) ProtoMessage() {}
 
 func (x *HelloRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_myapp_proto_msgTypes[0]
+	mi := &file_myapp_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -57,7 +152,7 @@ func (x *HelloRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use HelloRequest.ProtoReflect.Descriptor instead.
 func (*HelloRequest) Descriptor() ([]byte, []int) {
-	return file_myapp_proto_rawDescGZIP(), []int{0}
+	return file_myapp_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *HelloRequest) GetName() string {
@@ -65,6 +160,13 @@ func (x *HelloRequest) GetName() string {
 		return x.Name
 	}
 	return ""
+}
+
+func (x *HelloRequest) GetOld() int32 {
+	if x != nil {
+		return x.Old
+	}
+	return 0
 }
 
 type HelloResponse struct {
@@ -78,7 +180,7 @@ type HelloResponse struct {
 func (x *HelloResponse) Reset() {
 	*x = HelloResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_myapp_proto_msgTypes[1]
+		mi := &file_myapp_proto_msgTypes[3]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -91,7 +193,7 @@ func (x *HelloResponse) String() string {
 func (*HelloResponse) ProtoMessage() {}
 
 func (x *HelloResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_myapp_proto_msgTypes[1]
+	mi := &file_myapp_proto_msgTypes[3]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -104,7 +206,7 @@ func (x *HelloResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use HelloResponse.ProtoReflect.Descriptor instead.
 func (*HelloResponse) Descriptor() ([]byte, []int) {
-	return file_myapp_proto_rawDescGZIP(), []int{1}
+	return file_myapp_proto_rawDescGZIP(), []int{3}
 }
 
 func (x *HelloResponse) GetMessage() string {
@@ -118,17 +220,27 @@ var File_myapp_proto protoreflect.FileDescriptor
 
 var file_myapp_proto_rawDesc = []byte{
 	0x0a, 0x0b, 0x6d, 0x79, 0x61, 0x70, 0x70, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x05, 0x6d,
-	0x79, 0x61, 0x70, 0x70, 0x22, 0x22, 0x0a, 0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x71,
-	0x75, 0x65, 0x73, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01,
-	0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x22, 0x29, 0x0a, 0x0d, 0x48, 0x65, 0x6c, 0x6c,
-	0x6f, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73,
-	0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73,
-	0x61, 0x67, 0x65, 0x32, 0x45, 0x0a, 0x0f, 0x47, 0x72, 0x65, 0x65, 0x74, 0x69, 0x6e, 0x67, 0x53,
-	0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x32, 0x0a, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x12,
-	0x13, 0x2e, 0x6d, 0x79, 0x61, 0x70, 0x70, 0x2e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x71,
-	0x75, 0x65, 0x73, 0x74, 0x1a, 0x14, 0x2e, 0x6d, 0x79, 0x61, 0x70, 0x70, 0x2e, 0x48, 0x65, 0x6c,
-	0x6c, 0x6f, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42, 0x0a, 0x5a, 0x08, 0x70, 0x6b,
-	0x67, 0x2f, 0x67, 0x72, 0x70, 0x63, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x79, 0x61, 0x70, 0x70, 0x22, 0x1f, 0x0a, 0x0b, 0x56, 0x6f, 0x74, 0x65, 0x52, 0x65, 0x71, 0x75,
+	0x65, 0x73, 0x74, 0x12, 0x10, 0x0a, 0x03, 0x77, 0x68, 0x6f, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x03, 0x77, 0x68, 0x6f, 0x22, 0x26, 0x0a, 0x0c, 0x56, 0x6f, 0x74, 0x65, 0x52, 0x65, 0x73,
+	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x77, 0x69, 0x6e, 0x6e, 0x65, 0x72, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x77, 0x69, 0x6e, 0x6e, 0x65, 0x72, 0x22, 0x34, 0x0a,
+	0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x12, 0x0a,
+	0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d,
+	0x65, 0x12, 0x10, 0x0a, 0x03, 0x6f, 0x6c, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x03,
+	0x6f, 0x6c, 0x64, 0x22, 0x29, 0x0a, 0x0d, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x73, 0x70,
+	0x6f, 0x6e, 0x73, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x32, 0x45,
+	0x0a, 0x0f, 0x47, 0x72, 0x65, 0x65, 0x74, 0x69, 0x6e, 0x67, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63,
+	0x65, 0x12, 0x32, 0x0a, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x12, 0x13, 0x2e, 0x6d, 0x79, 0x61,
+	0x70, 0x70, 0x2e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a,
+	0x14, 0x2e, 0x6d, 0x79, 0x61, 0x70, 0x70, 0x2e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x73,
+	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x32, 0x40, 0x0a, 0x0b, 0x56, 0x6f, 0x74, 0x65, 0x53, 0x65, 0x72,
+	0x76, 0x69, 0x63, 0x65, 0x12, 0x31, 0x0a, 0x06, 0x56, 0x6f, 0x74, 0x69, 0x6e, 0x67, 0x12, 0x12,
+	0x2e, 0x6d, 0x79, 0x61, 0x70, 0x70, 0x2e, 0x56, 0x6f, 0x74, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65,
+	0x73, 0x74, 0x1a, 0x13, 0x2e, 0x6d, 0x79, 0x61, 0x70, 0x70, 0x2e, 0x56, 0x6f, 0x74, 0x65, 0x52,
+	0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42, 0x0a, 0x5a, 0x08, 0x70, 0x6b, 0x67, 0x2f, 0x67,
+	0x72, 0x70, 0x63, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -143,16 +255,20 @@ func file_myapp_proto_rawDescGZIP() []byte {
 	return file_myapp_proto_rawDescData
 }
 
-var file_myapp_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_myapp_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_myapp_proto_goTypes = []interface{}{
-	(*HelloRequest)(nil),  // 0: myapp.HelloRequest
-	(*HelloResponse)(nil), // 1: myapp.HelloResponse
+	(*VoteRequest)(nil),   // 0: myapp.VoteRequest
+	(*VoteResponse)(nil),  // 1: myapp.VoteResponse
+	(*HelloRequest)(nil),  // 2: myapp.HelloRequest
+	(*HelloResponse)(nil), // 3: myapp.HelloResponse
 }
 var file_myapp_proto_depIdxs = []int32{
-	0, // 0: myapp.GreetingService.Hello:input_type -> myapp.HelloRequest
-	1, // 1: myapp.GreetingService.Hello:output_type -> myapp.HelloResponse
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	2, // 0: myapp.GreetingService.Hello:input_type -> myapp.HelloRequest
+	0, // 1: myapp.VoteService.Voting:input_type -> myapp.VoteRequest
+	3, // 2: myapp.GreetingService.Hello:output_type -> myapp.HelloResponse
+	1, // 3: myapp.VoteService.Voting:output_type -> myapp.VoteResponse
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -165,7 +281,7 @@ func file_myapp_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_myapp_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*HelloRequest); i {
+			switch v := v.(*VoteRequest); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -177,6 +293,30 @@ func file_myapp_proto_init() {
 			}
 		}
 		file_myapp_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*VoteResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_myapp_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*HelloRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_myapp_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*HelloResponse); i {
 			case 0:
 				return &v.state
@@ -195,9 +335,9 @@ func file_myapp_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_myapp_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
-			NumServices:   1,
+			NumServices:   2,
 		},
 		GoTypes:           file_myapp_proto_goTypes,
 		DependencyIndexes: file_myapp_proto_depIdxs,
--- src/pkg/grpc/myapp_grpc.pb.go
+++ src/pkg/grpc/myapp_grpc.pb.go
@@ -103,3 +103,89 @@ var GreetingService_ServiceDesc = grpc.ServiceDesc{
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "myapp.proto",
 }
+
+// VoteServiceClient is the client API for VoteService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type VoteServiceClient interface {
+	Voting(ctx context.Context, in *VoteRequest, opts ...grpc.CallOption) (*VoteResponse, error)
+}
+
+type voteServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewVoteServiceClient(cc grpc.ClientConnInterface) VoteServiceClient {
+	return &voteServiceClient{cc}
+}
+
+func (c *voteServiceClient) Voting(ctx context.Context, in *VoteRequest, opts ...grpc.CallOption) (*VoteResponse, error) {
+	out := new(VoteResponse)
+	err := c.cc.Invoke(ctx, "/myapp.VoteService/Voting", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// VoteServiceServer is the server API for VoteService service.
+// All implementations must embed UnimplementedVoteServiceServer
+// for forward compatibility
+type VoteServiceServer interface {
+	Voting(context.Context, *VoteRequest) (*VoteResponse, error)
+	mustEmbedUnimplementedVoteServiceServer()
+}
+
+// UnimplementedVoteServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedVoteServiceServer struct {
+}
+
+func (UnimplementedVoteServiceServer) Voting(context.Context, *VoteRequest) (*VoteResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Voting not implemented")
+}
+func (UnimplementedVoteServiceServer) mustEmbedUnimplementedVoteServiceServer() {}
+
+// UnsafeVoteServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to VoteServiceServer will
+// result in compilation errors.
+type UnsafeVoteServiceServer interface {
+	mustEmbedUnimplementedVoteServiceServer()
+}
+
+func RegisterVoteServiceServer(s grpc.ServiceRegistrar, srv VoteServiceServer) {
+	s.RegisterService(&VoteService_ServiceDesc, srv)
+}
+
+func _VoteService_Voting_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(VoteRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(VoteServiceServer).Voting(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/myapp.VoteService/Voting",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(VoteServiceServer).Voting(ctx, req.(*VoteRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// VoteService_ServiceDesc is the grpc.ServiceDesc for VoteService service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var VoteService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "myapp.VoteService",
+	HandlerType: (*VoteServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Voting",
+			Handler:    _VoteService_Voting_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "myapp.proto",
+}
